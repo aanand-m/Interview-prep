c529cd787340fe435edf30f0df3e8790c493c5ed
Anand <66562526+aanand-m@users.noreply.github.com>
2025-02-15 16:00:25 +0530

    [242] [Time Beats: 36.77%] [Memory Beats: 44.92%] - LeetPush

--- 242-Valid-Anagram.java
+++ 242-Valid-Anagram.java
@@ -1,24 +1,16 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (t.length() != s.length()) return false;
-        Map <Character, Integer> mapS = new HashMap<>();
-        Map <Character, Integer> mapT = new HashMap<>();
-        int n = s.length();
-        for (int i = 0; i < n ; i++) {
-            char sChar = s.charAt(i);
-            char tChar = t.charAt(i);
-            if (mapS.containsKey(sChar)) {
-                mapS.put(sChar, mapS.get(sChar) + 1);
-            } else {
-                mapS.put(sChar, 1);
-            }
-
-            if (mapT.containsKey(tChar)) {
-                mapT.put(tChar, mapT.get(tChar) + 1);
-            } else {
-                mapT.put(tChar, 1);
-            }
-        }
-        return mapS.entrySet().equals(mapT.entrySet());
+        if (s.length() != t.length()) return false;
+        s = sortString(s);
+        t = sortString(t);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != t.charAt(i)) return false;
+        } 
+        return true;
+    }
+    public String sortString (String s) {
+        char[] c = s.toCharArray();
+        Arrays.sort(c);
+        return new String(c);
     }
 }
