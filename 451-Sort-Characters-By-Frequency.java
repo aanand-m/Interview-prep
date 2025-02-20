19d64f11e554939172641e5798742aeabfe56c58
Anand <66562526+aanand-m@users.noreply.github.com>
2025-02-20 21:15:28 +0530

    [451] [Time Beats: 98.85%] [Memory Beats: 90.39%] - LeetPush

--- 451-Sort-Characters-By-Frequency.java
+++ 451-Sort-Characters-By-Frequency.java
@@ -1,38 +1,28 @@
 class Solution {
-    StringBuilder ans = new StringBuilder();
     public String frequencySort(String s) {
-        NavigableMap<Integer, List <Character>> freq = new TreeMap<>();
-        Map<Character, Integer> charCount = new HashMap<>();
-
-        for (char c : s.toCharArray()) {
-            charCount.put(c, charCount.getOrDefault(c,0) + 1);
+        int[] freq = new int [256];
+        int n = s.length();
+        char[] charArr = new char[n];
+        for (char c : s.toCharArray()){
+            freq[c]++;
         }
+        for (int i = 0; i<n;) {
+            int max = 0;
+            int index = 0;
 
-        for(Map.Entry<Character, Integer> entry : charCount.entrySet()) {
-            char letter = entry.getKey();
-            int count = entry.getValue();
-
-            if (!freq.containsKey(count)) {
-                freq.put(count, new ArrayList<>());
+            for (int j = 0; j<256; j++) {
+                if (freq[j] > max) {
+                    max = freq[j];
+                    index = j;
+                }
             }
-            freq.get(count).add(letter);
-        }
 
-        for(Map.Entry<Integer,List<Character>> entry : freq.descendingMap().entrySet()) {
-            int count = entry.getKey();
-            List <Character> charList = entry.getValue();
-            for ( char c : charList) {
-                build(count, c);
+            while (freq[index] > 0) {
+                charArr[i++] = (char) index;
+                freq[index]--;
             }
+
         }
-
-        return ans.toString();
-
-    }
-
-    public void build (int count , char letter) {
-        for (int i =0 ; i<count; i++) {
-            ans.append(letter);
-        }
+        return new String(charArr);
     }
 }
