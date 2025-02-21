757525265102b854ce6963fa3b35fa9b268d5989
Anand <66562526+aanand-m@users.noreply.github.com>
2025-02-21 17:14:17 +0530

    [1781] [Time Beats: 37.56%] [Memory Beats: 60.41%] - LeetPush

---
+++ 1781-Sum-of-Beauty-of-All-Substrings.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maxCount(int[] freq) {
+        int max = 0;
+        for (int i = 0; i < 26; i++) {
+            max = Math.max(max, freq[i]);
+        }
+        return max;
+    }
+
+    public int minCount(int[] freq) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i< 26; i++) {
+            if (freq[i] != 0) {
+                min = Math.min(min, freq[i]);
+            }
+        }
+        return min;
+    }
+
+    public int beautySum(String s) {
+        int sum = 0;
+        for (int i = 0; i<s.length(); i++) {
+            int[] freq = new int[26];
+            for (int j = i; j<s.length(); j++) {
+                freq[s.charAt(j) - 'a']++;
+                int beauty = maxCount(freq) - minCount(freq);
+                sum+=beauty;
+            }
+        }
+        return sum;
+    }
+}
