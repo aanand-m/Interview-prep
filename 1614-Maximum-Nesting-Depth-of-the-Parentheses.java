bfac83984988ffbf6c4f24c984ec635837efade3
Anand <66562526+aanand-m@users.noreply.github.com>
2025-02-21 14:07:59 +0530

    [1614] [Time Beats: 100.00%] [Memory Beats: 62.08%] - LeetPush

--- 1614-Maximum-Nesting-Depth-of-the-Parentheses.java
+++ 1614-Maximum-Nesting-Depth-of-the-Parentheses.java
@@ -1,15 +1,13 @@
 class Solution {
     public int maxDepth(String s) {
-        int curr = 0;
         int max = 0;
+        int curr = 0;
         for (char c : s.toCharArray()) {
             if (c == '(') {
                 curr++;
                 max = Math.max(curr, max);
             }
-            else if ( c == ')') {
-                curr--;
-            }
+            else if (c == ')') curr--;
             else continue;
         }
         return max;
